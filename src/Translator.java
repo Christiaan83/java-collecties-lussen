601e594f49582813064216b0d0d7dd9c7f718de2
Christiaan83 <c.zielman@gmail.com>
2023-12-01 10:16:13 +0100

    Eerste begin gemaakt

---
+++ src/Translator.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+
+public class Translator {
+    private HashMap<Integer, String> numericAlpha;
+
+    public void TranslatorConstructor(String[] alphabetic, Integer[] numeric) {
+
+        for (int i = 0; i < numeric.length; i++){
+
+        }
+    }
+
+
+
+    }
+
+
+
